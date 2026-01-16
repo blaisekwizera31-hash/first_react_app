bdd65740536a3370445846ce0e95bbdd2ee2701a
blaisekwizera31-hash <blaisekwizera31@gmail.com>
2026-01-16 16:16:50 +0200

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
 import ListGroup from "./components/ListGroup";
 function App() {
   return (
-    <div>
-      <h1>List Group</h1>
+   <>      <h1>List Group</h1>
       <ul className="list-group">
         <li className="list-group-item">An item</li>
         <li className="list-group-item">A second item</li>
@@ -10,8 +9,7 @@ function App() {
         <li className="list-group-item">A fourth item</li>
         <li className="list-group-item">And a fifth one</li>
       </ul>
-      
-    </div>
+    </> 
         ); 
       }
     
---
+++ src/components/button.tsx
@@ -0,0 +1,8 @@
+function Button(){
+    return
+    <>
+    <button type="button" className="btn btn-secondary btn-lg">Large button</button>
+    </>
+}
+
+export default Button;
