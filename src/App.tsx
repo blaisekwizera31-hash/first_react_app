d2ab5e216a25a08ab1e3bef7293fb8c44c6b65c5
blaisekwizera31-hash <blaisekwizera31@gmail.com>
2026-01-16 15:29:54 +0200

    push

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import ListGroup
  from "./components/ListGroup";
 function App(){
-  return <ul class="list-group">
-  <li class="list-group-item">An item</li>
-  <li class="list-group-item">A second item</li>
-  <li class="list-group-item">A third item</li>
-  <li class="list-group-item">A fourth item</li>
-  <li class="list-group-item">And a fifth one</li>
+  return <ul className="list-group">
+  <li className="list-group-item">An item</li>
+  <li className="list-group-item">A second item</li>
+  <li className="list-group-item">A third item</li>
+  <li className="list-group-item">A fourth item</li>
+  <li className="list-group-item">And a fifth one</li>
 </ul>
 }
 
